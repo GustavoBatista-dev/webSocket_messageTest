178e4b4773ed413fec0703a48a669efd1443cdc5
GustavoBatista-dev <gustavob.anjos@gmail.com>
2024-05-01 15:35:20 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let messageId = 1; // keep track of the message id
+
+// listener for chat message from terminal
+rl.on("line", (input) => {
+  const message = {
+    _id: messageId++,
+    text: input, // Message text from terminal
+    createdAt: new Date(),
+    user: {
+      _id: "terminal", // User id for terminal input
+      name: "John Doe",
+      avatar: "https://i.pravatar.cc/150/",
+    },
+  };
+  io.emit("chat message", message);
+});
+
+io.on("connection", (socket) => {
+  console.log("a user connected", socket.id);
+
+  socket.on("chat message", (msg) => {
+    console.log("chat message received: ", msg);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("user disconnected", socket.id);
+  });
+});
+
+server.listen(8080, () => {
+  console.log("Listening to port 8080");
+});
